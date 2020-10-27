e3165f35ac3f14cfc0115d18e9455874d8cdc4fb
Dmitry Fedyuk <dfediuk@gmail.com>
2020-10-28 05:27:50 +0600

    0.4.0: https://github.com/tradefurniturecompany/site/issues/191

---
+++ Plugin/Catalog/Block/Product/View/GalleryOptions.php
@@ -0,0 +1,33 @@
+<?php
+namespace TFC\Core\Plugin\Catalog\Block\Product\View;
+use Magento\Catalog\Block\Product\View\Gallery as G;
+use Magento\Catalog\Block\Product\View\GalleryOptions as Sb;
+use Magento\Framework\DataObject as _DO;
+use Magento\Framework\Data\Collection as C;
+# 2020-10-28 "Improve the frontend product page": https://github.com/tradefurniturecompany/site/issues/191
+final class GalleryOptions {
+	/**
+	 * 2020-10-28
+	 * @see \Magento\Catalog\Block\Product\View\GalleryOptions::getOptionsJson()
+	 * @return string
+	 */
+	function afterGetOptionsJson(Sb $sb, string $r) {
+		$g = df_product_gallery_b(); /** @var G $g */
+		$gc = $g->getGalleryImages(); /** @var C $gc */
+		if ($gc->count()) {
+			/** @var _DO $i */
+			if (!($i = df_find($gc, function(_DO $i) use($g) {return $g->isMainImage($i);}))) {
+				$i = $gc->getFirstItem();
+			}
+			try {
+				$r = df_json_encode(
+					array_combine(['width', 'height'], array_slice(getimagesize(df_product_image_path2abs($i['file'])), 0, 2))
+					+ ['maxheight' => '60%']
+					+ df_json_decode($r)
+				);
+			}
+			catch (\Exception $e) {}
+		}
+		return $r;
+	}
+}
