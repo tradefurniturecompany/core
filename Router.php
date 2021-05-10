97d68649150d2e54fa724ace1e4578948d2791dd
Dmitry Fedyuk <dfediuk@gmail.com>
2021-05-11 05:38:40 +0600

    0.4.9: https://github.com/tradefurniturecompany/core/issues/40

---
+++ Router.php
@@ -0,0 +1,35 @@
+<?php
+namespace TFC\Core;
+use Magento\Framework\App\Action\Redirect;
+use Magento\Framework\App\RequestInterface as IRequest;
+use Magento\Framework\App\RouterInterface as IRouter;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite as Rewrite;
+# 2021-05-11 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+# "Redirect frontend product requests to canonical URLs (without a category path included)":
+# https://github.com/tradefurniturecompany/core/issues/40
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Router implements IRouter {
+	/**
+	 * 2021-05-11
+	 * @override
+	 * @see IRouter::match()
+	 * @used-by \Magento\Framework\App\FrontController::dispatch()
+	 * @param IRequest $req
+	 * @return Redirect|null
+	 */
+	function match(IRequest $req) {
+		$r = null; /** @var Redirect|null $r */
+		if (
+			($rew = df_url_finder()->findOneByData([
+				Rewrite::REQUEST_PATH => df_trim_ds_left($req->getPathInfo()), Rewrite::STORE_ID => df_store_id(),
+			])) /** @var Rewrite|null $rew */
+			&& 'product' === $rew->getEntityType()
+			&& 1 < count($parts = df_explode_url($rew->getRequestPath()))
+		) {
+			df_response()->setRedirect(df_url_frontend(df_last($parts)), 301);
+			$req->setDispatched(true);
+			$r = df_action_c_redirect();
+		}
+		return $r;
+	}
+}
