4a2a625997fd052361c47a3c835eacc46d3f0c6a
Dmitry Fedyuk <dfediuk@gmail.com>
2021-12-06 08:37:30 +0600

    0.5.3

--- Observer/CanLog.php
+++ Observer/CanLog.php
@@ -1,15 +1,15 @@
 <?php
 namespace TFC\Core\Observer;
-use Df\Framework\Logger\Handler as E;
+use Df\Framework\Log\Dispatcher as D;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 /**
- * 2020-08-30
+ * 2020-08-30, 2021-12-06
  * Event: `df_can_log`
- * @see \Df\Framework\Logger\Handler\System::handle()
- * 		df_dispatch('df_can_log', ['message' => $d, 'result' => ($o = new O)]); /** @var O $o
- * 		$r = $o[self::SKIP] || parent::handle($d);
- * https://github.com/mage2pro/core/blob/6.8.1/Framework/Logger/Handler/System.php#L44-L45
+ * @see \Df\Framework\Log\Dispatcher::handle()
+ * 		df_dispatch('df_can_log', ['message' => $d, 'result' => ($o = new O)]);
+ * 		if (!($r = !!$o[self::V_SKIP])) {
+ * https://github.com/mage2pro/core/blob/8.0.0/Framework/Log/Dispatcher.php#L54-L55
  */
 final class CanLog implements ObserverInterface {
 	/**
@@ -20,8 +20,8 @@ final class CanLog implements ObserverInterface {
 	 * @param Observer $ob
 	 */
 	function execute(Observer $ob) {
-		$m = $ob[E::P_MESSAGE]; /** @var array(string => mixed) $m */
-		$ob[E::P_RESULT][E::V_SKIP] = df_contains(dfa($m, 'message'), [
+		$m = $ob[D::P_MESSAGE]; /** @var array(string => mixed) $m */
+		$ob[D::P_RESULT][D::V_SKIP] = df_contains(dfa($m, 'message'), [
 			# 2020-08-30
 			# "Prevent Magento from logging the
 			# «PayPal NVP gateway errors: You do not have permissions to make this API call
